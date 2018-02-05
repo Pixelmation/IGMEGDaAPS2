4fed4e212610afa7e5875487b7bbbf0df2086e45
Nick Valcour <pixelation7.0@gmail.com>
2018-02-05 16:33:58 -0500

    Started my interface from hell

---
+++ Hell_Interface/Hell_Interface/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace Hell_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Name = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.Age1 = new System.Windows.Forms.RadioButton();
+            this.Age2 = new System.Windows.Forms.RadioButton();
+            this.Age3 = new System.Windows.Forms.RadioButton();
+            this.Age4 = new System.Windows.Forms.RadioButton();
+            this.Age5 = new System.Windows.Forms.RadioButton();
+            this.Age6 = new System.Windows.Forms.RadioButton();
+            this.Age7 = new System.Windows.Forms.RadioButton();
+            this.Age8 = new System.Windows.Forms.RadioButton();
+            this.Age9 = new System.Windows.Forms.RadioButton();
+            this.Age10 = new System.Windows.Forms.RadioButton();
+            this.Age11 = new System.Windows.Forms.RadioButton();
+            this.Age12 = new System.Windows.Forms.RadioButton();
+            this.Age13 = new System.Windows.Forms.RadioButton();
+            this.Age14 = new System.Windows.Forms.RadioButton();
+            this.Age15 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSkillsLabel = new System.Windows.Forms.TextBox();
+            this.textBoxSkills = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Font = new System.Drawing.Font("Niagara Engraved", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name.Location = new System.Drawing.Point(12, 45);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(46, 23);
+            this.Name.TabIndex = 0;
+            this.Name.Text = "Name:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Mistral", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxName.Location = new System.Drawing.Point(64, 51);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(33, 17);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // Age1
+            // 
+            this.Age1.AutoSize = true;
+            this.Age1.Location = new System.Drawing.Point(179, 51);
+            this.Age1.Name = "Age1";
+            this.Age1.Size = new System.Drawing.Size(37, 17);
+            this.Age1.TabIndex = 2;
+            this.Age1.TabStop = true;
+            this.Age1.Text = "18";
+            this.Age1.UseVisualStyleBackColor = true;
+            this.Age1.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age2
+            // 
+            this.Age2.AutoSize = true;
+            this.Age2.Location = new System.Drawing.Point(179, 74);
+            this.Age2.Name = "Age2";
+            this.Age2.Size = new System.Drawing.Size(37, 17);
+            this.Age2.TabIndex = 3;
+            this.Age2.TabStop = true;
+            this.Age2.Text = "19";
+            this.Age2.UseVisualStyleBackColor = true;
+            this.Age2.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age3
+            // 
+            this.Age3.AutoSize = true;
+            this.Age3.Location = new System.Drawing.Point(179, 97);
+            this.Age3.Name = "Age3";
+            this.Age3.Size = new System.Drawing.Size(37, 17);
+            this.Age3.TabIndex = 4;
+            this.Age3.TabStop = true;
+            this.Age3.Text = "20";
+            this.Age3.UseVisualStyleBackColor = true;
+            this.Age3.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age4
+            // 
+            this.Age4.AutoSize = true;
+            this.Age4.Location = new System.Drawing.Point(179, 120);
+            this.Age4.Name = "Age4";
+            this.Age4.Size = new System.Drawing.Size(37, 17);
+            this.Age4.TabIndex = 5;
+            this.Age4.TabStop = true;
+            this.Age4.Text = "21";
+            this.Age4.UseVisualStyleBackColor = true;
+            this.Age4.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age5
+            // 
+            this.Age5.AutoSize = true;
+            this.Age5.Location = new System.Drawing.Point(179, 143);
+            this.Age5.Name = "Age5";
+            this.Age5.Size = new System.Drawing.Size(37, 17);
+            this.Age5.TabIndex = 6;
+            this.Age5.TabStop = true;
+            this.Age5.Text = "22";
+            this.Age5.UseVisualStyleBackColor = true;
+            this.Age5.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age6
+            // 
+            this.Age6.AutoSize = true;
+            this.Age6.Location = new System.Drawing.Point(270, 51);
+            this.Age6.Name = "Age6";
+            this.Age6.Size = new System.Drawing.Size(37, 17);
+            this.Age6.TabIndex = 7;
+            this.Age6.TabStop = true;
+            this.Age6.Text = "24";
+            this.Age6.UseVisualStyleBackColor = true;
+            this.Age6.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age7
+            // 
+            this.Age7.AutoSize = true;
+            this.Age7.Location = new System.Drawing.Point(270, 74);
+            this.Age7.Name = "Age7";
+            this.Age7.Size = new System.Drawing.Size(37, 17);
+            this.Age7.TabIndex = 8;
+            this.Age7.TabStop = true;
+            this.Age7.Text = "17";
+            this.Age7.UseVisualStyleBackColor = true;
+            this.Age7.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age8
+            // 
+            this.Age8.AutoSize = true;
+            this.Age8.Location = new System.Drawing.Point(270, 97);
+            this.Age8.Name = "Age8";
+            this.Age8.Size = new System.Drawing.Size(37, 17);
+            this.Age8.TabIndex = 9;
+            this.Age8.TabStop = true;
+            this.Age8.Text = "25";
+            this.Age8.UseVisualStyleBackColor = true;
+            this.Age8.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age9
+            // 
+            this.Age9.AutoSize = true;
+            this.Age9.Location = new System.Drawing.Point(270, 120);
+            this.Age9.Name = "Age9";
+            this.Age9.Size = new System.Drawing.Size(37, 17);
+            this.Age9.TabIndex = 10;
+            this.Age9.TabStop = true;
+            this.Age9.Text = "26";
+            this.Age9.UseVisualStyleBackColor = true;
+            this.Age9.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age10
+            // 
+            this.Age10.AutoSize = true;
+            this.Age10.Location = new System.Drawing.Point(270, 143);
+            this.Age10.Name = "Age10";
+            this.Age10.Size = new System.Drawing.Size(37, 17);
+            this.Age10.TabIndex = 11;
+            this.Age10.TabStop = true;
+            this.Age10.Text = "27";
+            this.Age10.UseVisualStyleBackColor = true;
+            this.Age10.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age11
+            // 
+            this.Age11.AutoSize = true;
+            this.Age11.Location = new System.Drawing.Point(361, 51);
+            this.Age11.Name = "Age11";
+            this.Age11.Size = new System.Drawing.Size(37, 17);
+            this.Age11.TabIndex = 12;
+            this.Age11.TabStop = true;
+            this.Age11.Text = "31";
+            this.Age11.UseVisualStyleBackColor = true;
+            this.Age11.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age12
+            // 
+            this.Age12.AutoSize = true;
+            this.Age12.Location = new System.Drawing.Point(361, 74);
+            this.Age12.Name = "Age12";
+            this.Age12.Size = new System.Drawing.Size(37, 17);
+            this.Age12.TabIndex = 13;
+            this.Age12.TabStop = true;
+            this.Age12.Text = "33";
+            this.Age12.UseVisualStyleBackColor = true;
+            this.Age12.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age13
+            // 
+            this.Age13.AutoSize = true;
+            this.Age13.Location = new System.Drawing.Point(361, 97);
+            this.Age13.Name = "Age13";
+            this.Age13.Size = new System.Drawing.Size(37, 17);
+            this.Age13.TabIndex = 14;
+            this.Age13.TabStop = true;
+            this.Age13.Text = "32";
+            this.Age13.UseVisualStyleBackColor = true;
+            this.Age13.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age14
+            // 
+            this.Age14.AutoSize = true;
+            this.Age14.Location = new System.Drawing.Point(361, 120);
+            this.Age14.Name = "Age14";
+            this.Age14.Size = new System.Drawing.Size(37, 17);
+            this.Age14.TabIndex = 15;
+            this.Age14.TabStop = true;
+            this.Age14.Text = "34";
+            this.Age14.UseVisualStyleBackColor = true;
+            this.Age14.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // Age15
+            // 
+            this.Age15.AutoSize = true;
+            this.Age15.Location = new System.Drawing.Point(361, 143);
+            this.Age15.Name = "Age15";
+            this.Age15.Size = new System.Drawing.Size(37, 17);
+            this.Age15.TabIndex = 16;
+            this.Age15.TabStop = true;
+            this.Age15.Text = "42";
+            this.Age15.UseVisualStyleBackColor = true;
+            this.Age15.CheckedChanged += new System.EventHandler(this.Age_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Niagara Engraved", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 23);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Age:";
+            // 
+            // textBoxSkillsLabel
+            // 
+            this.textBoxSkillsLabel.Font = new System.Drawing.Font("Niagara Engraved", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSkillsLabel.Location = new System.Drawing.Point(16, 206);
+            this.textBoxSkillsLabel.Name = "textBoxSkillsLabel";
+            this.textBoxSkillsLabel.ReadOnly = true;
+            this.textBoxSkillsLabel.Size = new System.Drawing.Size(43, 30);
+            this.textBoxSkillsLabel.TabIndex = 18;
+            this.textBoxSkillsLabel.Text = "Skills";
+            // 
+            // textBoxSkills
+            // 
+            this.textBoxSkills.Font = new System.Drawing.Font("Mistral", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSkills.Location = new System.Drawing.Point(16, 251);
+            this.textBoxSkills.Multiline = true;
+            this.textBoxSkills.Name = "textBoxSkills";
+            this.textBoxSkills.ReadOnly = true;
+            this.textBoxSkills.Size = new System.Drawing.Size(79, 69);
+            this.textBoxSkills.TabIndex = 19;
+            this.textBoxSkills.Text = "None";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.textBoxSkills);
+            this.Controls.Add(this.textBoxSkillsLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Age15);
+            this.Controls.Add(this.Age14);
+            this.Controls.Add(this.Age13);
+            this.Controls.Add(this.Age12);
+            this.Controls.Add(this.Age11);
+            this.Controls.Add(this.Age10);
+            this.Controls.Add(this.Age9);
+            this.Controls.Add(this.Age8);
+            this.Controls.Add(this.Age7);
+            this.Controls.Add(this.Age6);
+            this.Controls.Add(this.Age5);
+            this.Controls.Add(this.Age4);
+            this.Controls.Add(this.Age3);
+            this.Controls.Add(this.Age2);
+            this.Controls.Add(this.Age1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.Name);
+            this.Text = "Job Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.RadioButton Age1;
+        private System.Windows.Forms.RadioButton Age2;
+        private System.Windows.Forms.RadioButton Age3;
+        private System.Windows.Forms.RadioButton Age4;
+        private System.Windows.Forms.RadioButton Age5;
+        private System.Windows.Forms.RadioButton Age6;
+        private System.Windows.Forms.RadioButton Age7;
+        private System.Windows.Forms.RadioButton Age8;
+        private System.Windows.Forms.RadioButton Age9;
+        private System.Windows.Forms.RadioButton Age10;
+        private System.Windows.Forms.RadioButton Age11;
+        private System.Windows.Forms.RadioButton Age12;
+        private System.Windows.Forms.RadioButton Age13;
+        private System.Windows.Forms.RadioButton Age14;
+        private System.Windows.Forms.RadioButton Age15;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSkillsLabel;
+        private System.Windows.Forms.TextBox textBoxSkills;
+    }
+}
+
---
+++ Hell_Interface/Hell_Interface/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hell_Interface
+{
+    public partial class Form1 : Form
+    {
+        //List of radio buttons
+        List<RadioButton> AgeButton = new List<RadioButton>();
+
+        //initialize rng
+        Random RNG = new Random();
+
+        //rng method
+        int RandNum()
+        {
+            int number = RNG.Next(1, 101);
+            return number;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //add radio buttons to list
+            #region radiobuttons
+            AgeButton.Add(Age1);
+            AgeButton.Add(Age2);
+            AgeButton.Add(Age3);
+            AgeButton.Add(Age4);
+            AgeButton.Add(Age5);
+            AgeButton.Add(Age6);
+            AgeButton.Add(Age7);
+            AgeButton.Add(Age8);
+            AgeButton.Add(Age9);
+            AgeButton.Add(Age10);
+            AgeButton.Add(Age11);
+            AgeButton.Add(Age12);
+            AgeButton.Add(Age13);
+            AgeButton.Add(Age14);
+            AgeButton.Add(Age15);
+            #endregion
+        }
+
+        private void Age_CheckedChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < AgeButton.Count; i++)
+            {
+                int newText = RandNum();
+                
+                AgeButton[i].Text = newText.ToString();
+            }
+        }
+    }
+}
