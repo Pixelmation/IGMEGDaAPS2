f26dbba85d2f1d41ba2d577a24a18a1608d0a338
Pixelmation <pixelation7.0@gmail.com>
2018-02-15 16:57:23 -0500

    just have to put everything in loops now

--- Collision Detection/Collision Detection/Game1.cs
+++ Collision Detection/Collision Detection/Game1.cs
@@ -23,7 +23,7 @@ namespace Collision_Detection
         //create a list for collecting object
         List<Rectangle> collect = new List<Rectangle>();
 
-        //texture and rectangle for collectable
+        //texture and rectangle for collectable, as well as positions for them
         Texture2D collectable;
         Rectangle collectableRect1;
         Rectangle collectableRect2;
@@ -31,6 +31,24 @@ namespace Collision_Detection
         Rectangle collectableRect4;
         Rectangle collectableRect5;
 
+        Vector2 position1;
+        Vector2 position2;
+        Vector2 position3;
+        Vector2 position4;
+        Vector2 position5;
+
+        //velocity for the rectangles to move at
+        Vector2 velocity1;
+        Vector2 velocity2;
+        Vector2 velocity3;
+        Vector2 velocity4;
+        Vector2 velocity5;
+
+        bool collected1 = false;
+        bool collected2 = false;
+        bool collected3 = false;
+        bool collected4 = false;
+        bool collected5 = false;
 
         public Game1()
         {
@@ -60,8 +78,12 @@ namespace Collision_Detection
             collect.Add(collectableRect4);
             collect.Add(collectableRect5);
 
-
-                base.Initialize();
+            velocity1 = new Vector2(VelocityX(), VelocityY());
+            velocity2 = new Vector2(VelocityX(), VelocityY());
+            velocity3 = new Vector2(VelocityX(), VelocityY());
+            velocity4 = new Vector2(VelocityX(), VelocityY());
+            velocity5 = new Vector2(VelocityX(), VelocityY());
+            base.Initialize();
         }
 
         /// <summary>
@@ -82,12 +104,20 @@ namespace Collision_Detection
                                             player.Width,
                                             player.Height);
 
-            //create locations for the collectable rectangles
-            collectableRect1 = new Rectangle(RandomWidth(), RandomHeight(), collectable.Width, collectable.Height);
-            collectableRect2 = new Rectangle(RandomWidth(), RandomHeight(), collectable.Width, collectable.Height);
-            collectableRect3 = new Rectangle(RandomWidth(), RandomHeight(), collectable.Width, collectable.Height);
-            collectableRect4 = new Rectangle(RandomWidth(), RandomHeight(), collectable.Width, collectable.Height);
-            collectableRect5 = new Rectangle(RandomWidth(), RandomHeight(), collectable.Width, collectable.Height);
+
+            //load the collectable and create locations for the collectable rectangles
+            collectable = Content.Load<Texture2D>("bread");
+            collectableRect1 = new Rectangle(CordX(), CordY(), collectable.Width, collectable.Height);
+            collectableRect2 = new Rectangle(CordX(), CordY(), collectable.Width, collectable.Height);
+            collectableRect3 = new Rectangle(CordX(), CordY(), collectable.Width, collectable.Height);
+            collectableRect4 = new Rectangle(CordX(), CordY(), collectable.Width, collectable.Height);
+            collectableRect5 = new Rectangle(CordX(), CordY(), collectable.Width, collectable.Height);
+
+            position1 = new Vector2(collectableRect1.X, collectableRect1.Y);
+            position2 = new Vector2(collectableRect2.X, collectableRect2.Y);
+            position3 = new Vector2(collectableRect3.X, collectableRect3.Y);
+            position4 = new Vector2(collectableRect4.X, collectableRect4.Y);
+            position5 = new Vector2(collectableRect5.X, collectableRect5.Y);
 
         }
 
@@ -112,6 +142,170 @@ namespace Collision_Detection
 
             // TODO: Add your update logic here
 
+            //changes each position by whatever it's velocity is every second
+            position1 += velocity1 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            position2 += velocity2 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            position3 += velocity3 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            position4 += velocity4 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            position5 += velocity5 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            //set the X and Y for each collectable rect
+            collectableRect1.X = (int)position1.X;
+            collectableRect1.Y = (int)position1.Y;
+
+            collectableRect2.X = (int)position2.X;
+            collectableRect2.Y = (int)position2.Y;
+
+            collectableRect3.X = (int)position3.X;
+            collectableRect3.Y = (int)position3.Y;
+
+            collectableRect4.X = (int)position4.X;
+            collectableRect4.Y = (int)position4.Y;
+
+            collectableRect5.X = (int)position5.X;
+            collectableRect5.Y = (int)position5.Y;
+
+            #region bounds check for collectableRect1
+            if (collectableRect1.Left <= 0 && velocity1.X < 0)
+            {
+                velocity1.X *= -1;
+            }
+            if (collectableRect1.Right >= GraphicsDevice.Viewport.Width && velocity1.X > 0)
+            {
+                velocity1.X *= -1;
+            }
+            if (collectableRect1.Top <= 0 && velocity1.Y < 0)
+            {
+                velocity1.Y *= -1;
+            }
+            if (collectableRect1.Bottom >= GraphicsDevice.Viewport.Height && velocity1.Y > 0)
+            {
+                velocity1.Y *= -1;
+            }
+            #endregion
+
+            #region bounds check for collectableRect2
+            if (collectableRect2.Left <= 0 && velocity2.X < 0)
+            {
+                velocity2.X *= -1;
+            }
+            if (collectableRect2.Right >= GraphicsDevice.Viewport.Width && velocity2.X > 0)
+            {
+                velocity2.X *= -1;
+            }
+            if (collectableRect2.Top <= 0 && velocity2.Y < 0)
+            {
+                velocity2.Y *= -1;
+            }
+            if (collectableRect2.Bottom >= GraphicsDevice.Viewport.Height && velocity2.Y > 0)
+            {
+                velocity2.Y *= -1;
+            }
+            #endregion
+
+            #region bounds check for collectableRect3
+            if (collectableRect3.Left <= 0 && velocity3.X < 0)
+            {
+                velocity3.X *= -1;
+            }
+            if (collectableRect3.Right >= GraphicsDevice.Viewport.Width && velocity3.X > 0)
+            {
+                velocity3.X *= -1;
+            }
+            if (collectableRect3.Top <= 0 && velocity3.Y < 0)
+            {
+                velocity3.Y *= -1;
+            }
+            if (collectableRect3.Bottom >= GraphicsDevice.Viewport.Height && velocity3.Y > 0)
+            {
+                velocity3.Y *= -1;
+            }
+            #endregion
+
+            #region bounds check for collectableRect4
+            if (collectableRect4.Left <= 0 && velocity4.X < 0)
+            {
+                velocity4.X *= -1;
+            }
+            if (collectableRect4.Right >= GraphicsDevice.Viewport.Width && velocity4.X > 0)
+            {
+                velocity4.X *= -1;
+            }
+            if (collectableRect4.Top <= 0 && velocity4.Y < 0)
+            {
+                velocity4.Y *= -1;
+            }
+            if (collectableRect4.Bottom >= GraphicsDevice.Viewport.Height && velocity4.Y > 0)
+            {
+                velocity4.Y *= -1;
+            }
+            #endregion
+
+            #region bounds check for collectableRect5
+            if (collectableRect5.Left <= 0 && velocity5.X < 0)
+            {
+                velocity5.X *= -1;
+            }
+            if (collectableRect5.Right >= GraphicsDevice.Viewport.Width && velocity5.X > 0)
+            {
+                velocity5.X *= -1;
+            }
+            if (collectableRect5.Top <= 0 && velocity5.Y < 0)
+            {
+                velocity5.Y *= -1;
+            }
+            if (collectableRect5.Bottom >= GraphicsDevice.Viewport.Height && velocity5.Y > 0)
+            {
+                velocity5.Y *= -1;
+            }
+            #endregion
+
+            //checks for collision. if collision, sets the appropriate collected bool to true
+            #region collision
+            if ((playerRect.Intersects(collectableRect1)) && (collected1 == false))
+            {
+                collected1 = true;
+            }
+
+            if ((playerRect.Intersects(collectableRect2)) && (collected2 == false))
+            {
+                collected2 = true;
+            }
+
+            if ((playerRect.Intersects(collectableRect3)) && (collected3 == false))
+            {
+                collected3 = true;
+            }
+
+            if ((playerRect.Intersects(collectableRect4)) && (collected4 == false))
+            {
+                collected4 = true;
+            }
+
+            if ((playerRect.Intersects(collectableRect5)) && (collected5 == false))
+            {
+                collected5 = true;
+            }
+            #endregion
+
+            KeyboardState state = Keyboard.GetState();
+            //movement with WASD
+            if (state.IsKeyDown(Keys.W))
+            {
+                playerRect.Y -= 5;
+            }
+            if (state.IsKeyDown(Keys.S))
+            {
+                playerRect.Y += 5;
+            }
+            if (state.IsKeyDown(Keys.A))
+            {
+                playerRect.X -= 5;
+            }
+            if (state.IsKeyDown(Keys.D))
+            {
+                playerRect.X += 5;
+            }
             base.Update(gameTime);
         }
 
@@ -121,27 +315,87 @@ namespace Collision_Detection
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Wheat);
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
+
             spriteBatch.Draw(player, playerRect, Color.White);
+
+            if (collected1 == false)
+                spriteBatch.Draw(collectable, collectableRect1, Color.White);
+
+            if (collected2 == false)
+                spriteBatch.Draw(collectable, collectableRect2, Color.White);
+
+            if (collected3 == false)
+                spriteBatch.Draw(collectable, collectableRect3, Color.White);
+
+            if (collected4 == false)
+                spriteBatch.Draw(collectable, collectableRect4, Color.White);
+
+            if (collected5 == false)
+                spriteBatch.Draw(collectable, collectableRect5, Color.White);
+
             spriteBatch.End();
 
             base.Draw(gameTime);
         }
-        
-        int RandomWidth()
+
+        #region random coords
+        /// <summary>
+        /// method for getting a random x cord
+        /// </summary>
+        /// <returns></returns>
+        int CordX()
         {
-            int width = RNG.Next(0,GraphicsDevice.Viewport.Width - collectable.Width);
+            int width = RNG.Next(20,GraphicsDevice.Viewport.Width - collectable.Width - 20);
             return width;
         }
 
-        int RandomHeight()
+        /// <summary>
+        /// method for getting a random y cord
+        /// </summary>
+        /// <returns></returns>
+        int CordY()
         {
-            int height = RNG.Next(0, GraphicsDevice.Viewport.Height - collectable.Height);
+            int height = RNG.Next(20, GraphicsDevice.Viewport.Height - collectable.Height -20);
             return height;
         }
+        #endregion
+
+        #region random velocity
+        /// <summary>
+        /// get a random x value between 100 and 200 for velocity
+        /// </summary>
+        /// <returns></returns>
+        int VelocityX()
+        {
+            int VX = RNG.Next(100,201);
+            int negativeX = RNG.Next(1, 3);
+            if (negativeX == 2)
+            {
+                VX *= -1;
+            }
+            return VX;
+        }
+
+        /// <summary>
+        /// get a random Y value between 100 and 200 for velocity
+        /// </summary>
+        /// <returns></returns>
+        int VelocityY()
+        {
+            int VY = RNG.Next(100, 201);
+            int negativeY = RNG.Next(1, 3);
+            if (negativeY == 2)
+            {
+                VY *= -1;
+            }
+            return VY;
+        }
+        #endregion
+
 
     }
 }
---
+++ Mario Walking PE/Mario Walking/Game1.cs
@@ -0,0 +1,320 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Mario_Walking
+{
+    // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    // PRACTICE EXERCISE:  Enums are typically declared here!
+
+    //create enum for mario
+    enum MarioState
+    {
+        FaceLeft,
+        FaceRight,
+        WalkLeft,
+        WalkRight
+    }
+    // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        MarioState MState = new MarioState();
+
+        // Texture and drawing
+        Texture2D spriteSheet;  // The single image with all of the animation frames
+        Vector2 marioLoc;       // Mario's location on the screen
+
+        // Animation
+        int frame;              // The current animation frame
+        double timeCounter;     // The amount of time that has passed
+        double fps;             // The speed of the animation
+        double timePerFrame;    // The amount of time (in fractional seconds) per frame
+
+        // Constants for "source" rectangle (inside the image)
+        const int WalkFrameCount = 3;       // The number of frames in the animation
+        const int MarioRectOffsetY = 116;   // How far down in the image are the frames?
+        const int MarioRectHeight = 72;     // The height of a single frame
+        const int MarioRectWidth = 44;      // The width of a single frame
+
+        // Constructor
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // Sets up the mario location
+            marioLoc = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+
+            // Initialize
+            fps = 10.0;
+            timePerFrame = 1.0 / fps;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Load the single spritesheet
+            spriteSheet = Content.Load<Texture2D>("Mario");
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            KeyboardState state = Keyboard.GetState();
+
+            // Handles animation for you
+            UpdateAnimation(gameTime);
+
+            // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+            // PRACTICE EXERCISE: Add your finite state machine code (switch statement) here!
+            // - Be sure to check the finite state machine's state first
+            // - Then check for specific transitions inside each state (may require keyboard input)
+
+            //switch statement for checking each state
+            #region the switch statement for changing state
+            switch (MState)
+            {
+                //if mario is facing left
+                case MarioState.FaceLeft:
+                    //if left arrow is held, change state to walkleft
+                    if (state.IsKeyDown(Keys.Left))
+                    {
+                        MState = MarioState.WalkLeft;
+
+                    }
+
+                    //if right arrow is held, change state to walkright
+                    if (state.IsKeyDown(Keys.Right))
+                    {
+                        MState = MarioState.WalkRight;
+                    }
+                    break;
+
+                //if mario is facing right
+                case MarioState.FaceRight:
+                    //if left arrow is held, change state to walkleft
+                    if (state.IsKeyDown(Keys.Left))
+                    {
+                        MState = MarioState.WalkLeft;
+                    }
+
+                    //if right arrow is held, change state to walkright
+                    if (state.IsKeyDown(Keys.Right))
+                    {
+                        MState = MarioState.WalkRight;
+                    }
+                    break;
+
+                //if mario is walking left
+                case MarioState.WalkLeft:
+
+                    //if neither key is held, change state to faceleft
+                    if (state.IsKeyUp(Keys.Left) && state.IsKeyUp(Keys.Right))
+                    {
+                        MState = MarioState.FaceLeft;
+                    }
+
+                    //if left arrow is up and right arrow is down, change state to walkright
+                    if (state.IsKeyUp(Keys.Left) && state.IsKeyDown(Keys.Right))
+                    {
+                        MState = MarioState.WalkRight;
+                    }
+                    break;
+
+
+                case MarioState.WalkRight:
+                    //if neither key is held, change state to faceright
+                    if (state.IsKeyUp(Keys.Left) && state.IsKeyUp(Keys.Right))
+                    {
+                        MState = MarioState.FaceRight;
+                    }
+
+                    //if right arrow is up and left arrow is down, change state to walkleft
+                    if (state.IsKeyUp(Keys.Right) && state.IsKeyDown(Keys.Left))
+                    {
+                        MState = MarioState.WalkLeft;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            #endregion
+
+            //if MState is WalkLeft, move left 5 pixels. if it's WalkRight, then move right 2 pixels. 
+            //If LeftShift is held, add/subtract another 4 pixels(sprint check)
+            #region the switch statements for moving
+            switch (MState)
+            {
+                case MarioState.WalkLeft:
+                    marioLoc.X -= 2;
+                    if (state.IsKeyDown(Keys.LeftShift))
+                    {
+                        marioLoc.X -= 4;
+                    }
+                    break;
+                case MarioState.WalkRight:
+                    marioLoc.X += 2;
+                    if (state.IsKeyDown(Keys.LeftShift))
+                    {
+                        marioLoc.X += 4;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            #endregion
+
+            // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // Begin the sprite batch
+            spriteBatch.Begin();
+
+            // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+            // PRACTICE EXERCISE: Check the finite state machine state here to
+            // determine how exactly to draw Mario
+
+            //Draws mario depending on his state
+            #region draw switch
+            switch (MState)
+            {
+                case MarioState.FaceLeft:
+                    DrawMarioStanding(SpriteEffects.FlipHorizontally);
+                    break;
+                case MarioState.FaceRight:
+                    DrawMarioStanding(SpriteEffects.None);
+                    break;
+                case MarioState.WalkLeft:
+                    DrawMarioWalking(SpriteEffects.FlipHorizontally);
+                    break;
+                case MarioState.WalkRight:
+                    DrawMarioWalking(SpriteEffects.None);
+                    break;
+                default:
+                    break;
+            }
+            #endregion
+
+            // *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+
+            // End the sprite batch
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// Updates mario's animation as necessary
+        /// </summary>
+        /// <param name="gameTime">Time information</param>
+        private void UpdateAnimation(GameTime gameTime)
+        {
+            // Handle animation timing
+            // - Add to the time counter
+            // - Check if we have enough "time" to advance the frame
+            timeCounter += gameTime.ElapsedGameTime.TotalSeconds;
+            if (timeCounter >= timePerFrame)
+            {
+                frame += 1;                     // Adjust the frame
+
+                if (frame > WalkFrameCount)     // Check the bounds
+                    frame = 1;                  // Back to 1 (since 0 is the "standing" frame)
+
+                timeCounter -= timePerFrame;    // Remove the time we "used"
+
+            }
+        }
+
+        /// <summary>
+        /// Draws only the left-most frame of mario, which is him standing
+        /// </summary>
+        /// <param name="flipSprite">
+        /// Enum values for flipping the sprite horizontally
+        /// and or vertically
+        /// </param>
+        private void DrawMarioStanding(SpriteEffects flipSprite)
+        {
+            spriteBatch.Draw(
+                spriteSheet,                    // - The texture to draw
+                marioLoc,                       // - The location to draw on the screen
+                new Rectangle(                  // - The "source" rectangle
+                    0,                          //   - This rectangle specifies
+                    MarioRectOffsetY,        //	   where "inside" the texture
+                    MarioRectWidth,           //     to get pixels (We don't want to
+                    MarioRectHeight),         //     draw the whole thing)
+                Color.White,                    // - The color
+                0,                              // - Rotation (none currently)
+                Vector2.Zero,                   // - Origin inside the image (top left)
+                1.0f,                           // - Scale (100% - no change)
+                flipSprite,                     // - Can be used to flip the image
+                0);                             // - Layer depth (unused)
+        }
+
+        /// <summary>
+        /// Draws mario running, based on the current FRAME field
+        /// which is changed by the code in Update
+        /// </summary>
+        /// <param name="flipSprite">
+        /// Enum values for flipping the sprite horizontally
+        /// and or vertically
+        /// </param>
+        private void DrawMarioWalking(SpriteEffects flipSprite)
+        {
+            spriteBatch.Draw(
+                spriteSheet,                    // - The texture to draw
+                marioLoc,                       // - The location to draw on the screen
+                new Rectangle(                  // - The "source" rectangle
+                    frame * MarioRectWidth,     //   - This rectangle specifies
+                    MarioRectOffsetY,           //	   where "inside" the texture
+                    MarioRectWidth,             //     to get pixels (We don't want to
+                    MarioRectHeight),           //     draw the whole thing)
+                Color.White,                    // - The color
+                0,                              // - Rotation (none currently)
+                Vector2.Zero,                   // - Origin inside the image (top left)
+                1.0f,                           // - Scale (100% - no change)
+                flipSprite,                     // - Can be used to flip the image
+                0);                             // - Layer depth (unused)
+        }
+    }
+}
