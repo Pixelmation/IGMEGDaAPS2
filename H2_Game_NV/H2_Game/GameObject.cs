ae096aa575895042118a7a2633ba2616fd74c01f
Pixelmation <pixelation7.0@gmail.com>
2018-02-20 01:48:29 -0500

    Fixed it all

--- H2_Game_NV/H2_Game/GameObject.cs
+++ H2_Game_NV/H2_Game/GameObject.cs
@@ -14,57 +14,37 @@ namespace H2_Game
     {
         Random RNG = new Random();
 
-        //generic texture for the sprites, vector for location and rectangle for the position
         public Texture2D spriteTexture { get; set; }
-        public Vector2 location { get; set; }
         public Rectangle position { get; set; }
 
-        //generic ints for the sprites width and height
         public int spriteWidth { get; set; }
         public int spriteHeight { get; set; }
 
-        //current frame and frame count variables
         public int currentFrame { get; set; }
         public int frameCount { get; set; }
 
-        //a time for each frame, a timer that it uses, and a spritespeed
         public int timePerFrame { get; set; }
         public float timer { get; set; }
         public int spriteSpeed { get; set; }
+        public Vector2 location { get; set; }
 
-        /// <summary>
-        /// generic draw method
-        /// </summary>
-        /// <param name="spriteBatch"></param>
         public virtual void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(spriteTexture, position, Color.White);
         }
 
-        /// <summary>
-        /// random X coordinate between 0 and 935
-        /// </summary>
-        /// <returns></returns>
         protected int XCoord()
         {
             int cord = RNG.Next(0, 935);
             return cord;
         }
 
-        /// <summary>
-        /// random Y coordinate between 0 and 400
-        /// </summary>
-        /// <returns></returns>
         protected int YCoord()
         {
             int cord = RNG.Next(0, 400);
             return cord;
         }
 
-        /// <summary>
-        /// random int between 1 and 2 for velocities
-        /// </summary>
-        /// <returns></returns>
         protected int speed()
         {
             int num = RNG.Next(1, 3);
