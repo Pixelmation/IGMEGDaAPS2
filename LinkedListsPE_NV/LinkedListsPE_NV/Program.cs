d1114b5823b38dc37cb035a24ca13bb4207bb327
Nick Valcour <pixelation7.0@gmail.com>
2018-03-26 16:30:27 -0400

    Making A Linked List

---
+++ LinkedListsPE_NV/LinkedListsPE_NV/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListsPE_NV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList CL = new CustomLinkedList();
+            CL.Add("Magic The Gathering");
+            CL.Add("Pokemon");
+            CL.Add("Yugioh");
+            CL.Add("Yu-Gi-Oh");
+            CL.Add("Vanguard");
+
+            while (true)
+            {
+                Console.Write("please choose a number between 1 and 5 to get the index of: ");
+                string answer = Console.ReadLine();
+                int answer2 = -1;
+                int.TryParse(answer, out answer2);
+                CL.GetData(answer2);
+                Console.WriteLine();
+            }
+        }
+    }
+}
