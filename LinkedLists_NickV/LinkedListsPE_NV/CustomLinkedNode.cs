7072aef71b484c6c0bde649171b5e6f70499fdf3
Pixelmation <pixelation7.0@gmail.com>
2018-04-03 21:04:41 -0400

    uhhh

---
+++ LinkedLists_NickV/LinkedListsPE_NV/CustomLinkedNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListsPE_NV
+{
+    class CustomLinkedNode
+    {        
+        
+        //data and its property
+        string data;
+        public string Data { get => data; set => data = value; }
+
+        //next and it's property
+        CustomLinkedNode next;
+        public CustomLinkedNode Next { get => next; set => next = value; }
+
+        /// <summary>
+        /// constructor that sets Data and Next to default values
+        /// </summary>
+        /// <param name="data"></param>
+        public CustomLinkedNode(string data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
---
+++ LinkedLists_NickV/LinkedListsPE_NV/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListsPE_NV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate CustomLinkedList and add 5 items
+            CustomLinkedList CL = new CustomLinkedList();
+            CL.Add("Magic The Gathering");
+            CL.Add("Pokemon");
+            CL.Add("Yugioh");
+            CL.Add("Yu-Gi-Oh");
+            CL.Add("Vanguard");
+
+            /*
+            //allow to the user to ask repeatedly for data.
+            while (true)
+            {
+                Console.Write("please choose a number between 1 and {0} to get the index of: ", CL.Count);
+                string answer = Console.ReadLine();
+                int answer2 = -1;
+                int.TryParse(answer, out answer2);
+                Console.WriteLine(CL.GetData(answer2));
+            }
+            */
+
+            //cycle through each node and print it
+            for (int i = 0; i < CL.Count; i++)
+            {
+                Console.WriteLine((i+1) + ": " + CL.GetData(i+1));
+            }
+        }
+    }
+}
