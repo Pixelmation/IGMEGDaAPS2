e15c856959854791df047dbdce66bfb33fe01a30
Nick Valcour <pixelation7.0@gmail.com>
2018-04-04 16:23:32 -0400

    making a tree

---
+++ TalentTree_PE/TalentTree_PE/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentTree_PE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Level 1
+            TalentTreeNode Swordsman = new TalentTreeNode("Swordsman", true, null, null);
+
+            //Level 2
+            TalentTreeNode Fighter = new TalentTreeNode("Fighter", true, null, null);
+            TalentTreeNode Rouge = new TalentTreeNode("Rouge", true, null, null);
+
+            //Level 3 Left
+            TalentTreeNode Knight = new TalentTreeNode("Knight", false, null, null);
+            TalentTreeNode Barbarian = new TalentTreeNode("Barbarian", false, null, null);
+
+            //Level 3 Right
+            TalentTreeNode Theif = new TalentTreeNode("Theif", true, null, null);
+            TalentTreeNode Assassin = new TalentTreeNode("Assassin", false, null, null);
+
+
+            //set nodes for swordsman
+            Swordsman.LeftNode = Fighter;
+            Swordsman.RightNode = Rouge;
+
+            //set nodes for Fighter
+            Fighter.LeftNode = Knight;
+            Fighter.RightNode = Barbarian;
+
+            //set nodes for Rouge
+            Rouge.LeftNode = Theif;
+            Rouge.RightNode = Assassin;
+            
+        }
+    }
+}
---
+++ TalentTree_PE/TalentTree_PE/TalentTreeNode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalentTree_PE
+{
+    class TalentTreeNode
+    {
+        //Name and it's property
+        string name;
+        public string Name { get => name; set => name = value; }
+
+        //Learned and it's property
+        bool learned;
+        public bool Learned { get => learned; set => learned = value; }
+
+        //LeftNode and it's property
+        TalentTreeNode leftNode;
+        public TalentTreeNode LeftNode { get => leftNode; set => leftNode = value; }
+
+        //RightNode and it's property
+        TalentTreeNode rightNode;
+        public TalentTreeNode RightNode { get => rightNode; set => rightNode = value; }
+
+        public TalentTreeNode(string name, bool learned, TalentTreeNode leftNode, TalentTreeNode rightNode)
+        {
+            Name = name;
+            Learned = learned;
+            LeftNode = leftNode;
+            RightNode = rightNode;
+        }
+
+
+        public void ListAllAbilities()
+        {
+            Console.WriteLine("All possible classes:");
+
+        }
+
+        public void ListKnownAbilities()
+        {
+
+        }
+
+        public void ListPossibleAbilities()
+        {
+
+        }
+    }
+}
