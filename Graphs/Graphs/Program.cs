b93f8542f6a9c7633152966b7c5296e07fdcaf81
Nick Valcour <pixelation7.0@gmail.com>
2018-04-16 16:32:34 -0400

    Making a thing

---
+++ Graphs/Graphs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = new Graph(null);
+            string currentRoom = "Kitchen";
+
+            while (!false)
+            {
+                Console.WriteLine("\nYou are currently in the " + currentRoom);
+                Console.Write("Nearby are:      ");
+                for (int i = 0; i < graph.GetAdjacentList(currentRoom).Count; i++)
+                {
+                    Console.Write(graph.GetAdjacentList(currentRoom)[i] + "     ");
+                }
+                Console.Write("\nWhere would you like to go? ");
+                string answer = Console.ReadLine();
+                if (graph.IsConnected(currentRoom, answer))
+                {
+                    if (answer == "Exit")
+                    {
+                        Console.WriteLine("You have successfully Escaped!");
+                        return;
+                    }
+                    currentRoom = answer;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, that is not an entry.");
+                }
+            }
+
+        }
+    }
+}
